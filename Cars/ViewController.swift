f052aa5c90574091c8fa62a7d0f82c1507f5df41
christina <blueberrrycake@gmail.com>
2019-10-14 16:10:41 +0300

    Initial Commit

---
+++ Cars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cars
+//
+//  Created by zombewmew on 14/10/2019.
+//  Copyright © 2019 Christina S. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
